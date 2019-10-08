8d9835c925e87b6081549355ab684ad49c98ecd1
Tochituru <rotrupo@gmail.com>
2019-10-08 19:53:19 -0300

    Fijar un estado onChange

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import { CssBaseline, Container } from '@material-ui/core'
 import TextField from '@material-ui/core/TextField'
 class App extends React.Component {
   state = {
-    task: '',
+    task: 'texto precargado',
     todo: []
   }
+
+  fieldHandler(e) {
+    this.setState({task: e.target.value})
+    console.log(e.target.value);
+  }
+
   render() {
     return (
       <Container>
         <CssBaseline />
         <h1> Todo</h1>
-        <TextField label={'task'} name={'task'} variant='outlined' />
+        <TextField 
+        value={this.state.task} 
+        label={'task'} 
+        name={'task'} 
+        onChange={e=>this.fieldHandler(e)} 
+        variant='outlined' />
       </Container>
     )
   }
