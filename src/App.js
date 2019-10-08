cc51f675c8692a18238939095d38b1a4c61c1303
Tochituru <rotrupo@gmail.com>
2019-10-08 19:27:17 -0300

    Agregamos el estado

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { CssBaseline, Container } from '@material-ui/core'
 
 class App extends React.Component {
+  state = {
+    task: '',
+    todo: []
+  }
   render() {
     return (
-    <Container>
-      <CssBaseline/>
-    <h1> Todo</h1>;
-    </Container>
-  )}
+      <Container>
+        <CssBaseline />
+        <h1> Todo</h1>
+      </Container>
+    )
+  }
 }
 
 export default App;
